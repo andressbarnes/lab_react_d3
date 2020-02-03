bb6f23699cc291d84c50382f44a6920a67534ea7
andressbarnes <andressbarnes@gmail.com>
2020-02-03 11:17:31 -0800

    refactor to make external data call

--- src/components/D3Chart.js
+++ src/components/D3Chart.js
@@ -1,6 +1,6 @@
 import * as d3 from 'd3'; //TODO refine import later
 
-const data = [20, 12, 16, 5, 30];
+const url = 'https://udemy-react-d3.firebaseio.com/ages.json';
 
 export default class D3Chart {
   constructor(element) {
@@ -10,15 +10,19 @@ export default class D3Chart {
       .attr('width', 500)
       .attr('height', 500);
 
-    const rects = svg.selectAll('rect').data(data);
+    d3.json(url).then(agesData => {
+      const rects = svg.selectAll('rect').data(agesData);
 
-    rects
-      .enter()
-      .append('rect')
-      .attr('x', (d, i) => i * 50)
-      .attr('y', 50)
-      .attr('width', 20)
-      .attr('height', d => 100 + d)
-      .attr('fill', 'grey');
+      rects
+        .enter()
+        .append('rect')
+        .attr('x', (d, i) => i * 50)
+        .attr('y', 50)
+        .attr('width', 20)
+        .attr('height', d => 100 + d.age * 4)
+        .attr('fill', d => {
+          return d.age > 10 ? 'red' : 'green';
+        });
+    });
   }
 }
