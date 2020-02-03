5cc38dc842a31d9d8343ae73305445d170653917
andressbarnes <andressbarnes@gmail.com>
2020-02-03 10:39:23 -0800

    add data joins to chart

--- src/components/D3Chart.js
+++ src/components/D3Chart.js
@@ -1,5 +1,7 @@
 import * as d3 from 'd3'; //TODO refine import later
 
+const data = [20, 12, 16, 5, 30];
+
 export default class D3Chart {
   constructor(element) {
     const svg = d3
@@ -8,12 +10,15 @@ export default class D3Chart {
       .attr('width', 500)
       .attr('height', 500);
 
-    svg
+    const rects = svg.selectAll('rect').data(data);
+
+    rects
+      .enter()
       .append('rect')
-      .attr('x', 50)
+      .attr('x', (d, i) => i * 50)
       .attr('y', 50)
-      .attr('width', 100)
-      .attr('height', 100)
+      .attr('width', 20)
+      .attr('height', d => 100 + d)
       .attr('fill', 'grey');
   }
 }
